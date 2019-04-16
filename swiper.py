aeb9e596f410cc84eaaffa1d11e41e168bc0f728
Daniel G Gonzalez Cunningham <dangonzo@mit.edu>
2019-04-15 21:11:34 -0400

    Code now stops swiping when there aren't more people to swipe on

--- swiper.py
+++ swiper.py
@@ -3,13 +3,14 @@ import time
 from selenium import webdriver
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
 
 
 class Swiper:
     def __init__(self):
         #self.driver = webdriver.Firefox('./geckodriver')
         self.driver = webdriver.Chrome()
-        self.driver.implicitly_wait(5) # Give 5 seconds for things to run before error-ing out
+        # self.driver.implicitly_wait(5) # Give 5 seconds for things to run before error-ing out
         self.login()
 
     def login(self):
@@ -28,17 +29,22 @@ class Swiper:
     def start_swiping(self):
         input('When you\'re logged in and ready to swipe, press any key')
         
+        sad_boi_text = '//*[@id="main"]/div/div[1]/main/div[2]/article/div/span/section/div/h1/span'
         love_is_in_the_air = True
-
+        
         # Yeah, I know, no while loops, but oh well
         while love_is_in_the_air:
             time.sleep(1)
-            # NOTE: Write code to make it so that the thing stops when you run out of people to swipe on
-            # sad_boi_text = '//*[@id="main"]/div/div[1]/main/div[2]/article/div/span/section/div/h1/span'
-            # yeet = self.driver.find_element_by_xpath(sad_boi_text)
-            # print(yeet)
+            try:
+                # The `sad_boi_text` xpath represents what you'd see when there
+                # aren't any more people to swipe on
+                self.driver.find_element_by_xpath(sad_boi_text)
+                love_is_in_the_air = False
+            except NoSuchElementException as e:
+                ActionChains(self.driver).send_keys(Keys.ARROW_RIGHT).perform()
 
-            ActionChains(self.driver).send_keys(Keys.ARROW_RIGHT).perform()
+        print('Done, no more people to swipe on. Come back later.')
+
 
 if __name__ == "__main__":
     # run the thing
